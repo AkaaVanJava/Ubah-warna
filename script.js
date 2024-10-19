a17c2fa0d6baeece613153c958d13941fb2edfa7
AkaaVanJava <139239411+AkaaVanJava@users.noreply.github.com>
2024-10-19 20:54:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const body = document.body;
+
+// const button = document.getElementById("button");
+// button.addEventListener("click", function () {
+//   alert("tombol di tekan");
+// });
+
+const btn2 = document.getElementById("btn2");
+
+function red() {
+  body.style.background = "red";
+}
+
+function yellow() {
+  body.style.background = "yellow";
+}
+
+function green() {
+  body.style.background = "rgb(0, 255, 0)";
+}
+
+function std() {
+  body.style.background = "#fff";
+}
